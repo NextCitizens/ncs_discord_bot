8c68ee7a20bb000b18551b817079d9348e72cfe0
foltone <aug.viel@gmail.com>
2022-05-30 14:58:20 +0200

    :package: add mut and unmute

---
+++ slscommands/moderation/mute.js
@@ -0,0 +1,52 @@
+const {
+    Client,
+    CommandInteraction,
+    MessageEmbed,
+    MessageActionRow,
+    MessageButton,
+    Message,
+} = require("discord.js");
+const wait = require("util").promisify(setTimeout);
+const ms = require('ms');
+
+module.exports = {
+    name: "mute",
+    description: "Mute a user for a duration with a reason!",
+    permissions: ["MODERATE_MEMBERS"],
+    type: 1,
+    options: [
+        {
+            name: 'target',
+            description: 'User to mute',
+            type: 'USER',
+            required: true
+        },
+        {
+            name: 'duration',
+            description: 'Duration of mute',
+            type: 'STRING',
+            required: true
+        },
+        {
+            name: 'reason',
+            description: 'Reason of mute',
+            type: 'STRING',
+            required: true
+        }
+    ],
+
+    run: async (client, interaction, args) => {
+        const target = interaction.options.getMember('target');
+        const duration = interaction.options.getString('duration')
+        const convertedTime = ms(duration);
+        const reason = interaction.options.getString('reason');
+
+        if (!target.moderatable) return interaction.editReply('This user cannot be muted!');
+        if (!convertedTime) return interaction.editReply('Specify a valid duration!')
+
+        target.timeout(convertedTime, reason);
+        
+        await interaction.editReply(`User ${target} was successfully muted, during : ${duration}, for: ${reason}`)
+        
+    },
+};
---
+++ slscommands/moderation/unmute.js
@@ -0,0 +1,36 @@
+const {
+    Client,
+    CommandInteraction,
+    MessageEmbed,
+    MessageActionRow,
+    MessageButton,
+    Message,
+} = require("discord.js");
+const wait = require("util").promisify(setTimeout);
+const ms = require('ms');
+
+module.exports = {
+    name: "unmute",
+    description: "Unmute a user!",
+    permissions: ["MODERATE_MEMBERS"],
+    type: 1,
+    options: [
+        {
+            name: 'target',
+            description: 'User to unmute',
+            type: 'USER',
+            required: true
+        }
+    ],
+
+    run: async (client, interaction, args) => {
+    
+        const target = interaction.options.getMember('target');
+    
+        if (!target.isCommunicationDisabled()) return interaction.editReply('This user cannot be unmuted!');
+
+        target.timeout(null);
+        await interaction.editReply(`User ${target} was successfully unmuted!`)
+        
+    },
+};
